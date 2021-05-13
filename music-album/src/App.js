286b8eea8f1a50570adce23b42c045a5a9e3871b
prajapatilalit <prajapatilalit150992@gmail.com>
2021-05-14 01:36:47 +0530

    initial setup of album

---
+++ music-album/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Album from "./Components/Album/Album";
+function App() {
+  return <Album />;
+}
+
+export default App;
---
+++ music-album/src/Components/Album/Album.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Album = () => {
+  return <h1>Album Gallary</h1>;
+};
+
+export default Album;
