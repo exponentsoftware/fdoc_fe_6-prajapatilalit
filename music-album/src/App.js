775421c6810baf25bc4d6be5cc89df181cf560b9
prajapatilalit <prajapatilalit150992@gmail.com>
2021-05-14 10:46:23 +0530

    create and update album component

--- music-album/src/App.js
+++ music-album/src/App.js
@@ -1,7 +1,45 @@
 import React from "react";
 import Album from "./Components/Album/Album";
 function App() {
-  return <Album />;
+  return (
+    <>
+      <div className="container">
+        <div className="albums">
+          <h1 className="album-heading">Albums</h1>
+          <Album
+            album_cover="https://stat2.bollywoodhungama.in/wp-content/uploads/2016/03/Rockstar-Poster-Feature-306x393.jpg"
+            album_title="Rockstar"
+            artist="A.R. Rehman"
+          />
+          <Album
+            album_cover="https://static-musique.qub.ca/images/covers/20/61/0060253726120_max.jpg"
+            album_title="Diamond"
+            artist="Rihana"
+          />
+          <Album
+            album_cover="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQHiFZH8kKxbCZRbElcmmW1py65EmiqpqWk6LBEuEH76kII_xcx"
+            album_title="Jodha Akbar"
+            artist="A.R. Rehman"
+          />
+          <Album
+            album_cover="https://in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/highway-et00014920-17-06-2020-12-28-06.jpg"
+            album_title="Highway"
+            artist="A.R. Rehman"
+          />
+          <Album
+            album_cover="https://i.ytimg.com/vi/lfmYmTpIH4o/movieposter.jpg"
+            album_title="Ranjhana"
+            artist="A.R. Rehman"
+          />
+          <Album
+            album_cover="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTAY3NEEcdyk9xbinmK_PgI0zX5otw3-xkTmvvDHSHoN1Q6MrYk"
+            album_title="Dil Bechara"
+            artist="A.R. Rehman"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- music-album/src/Components/Album/Album.js
+++ music-album/src/Components/Album/Album.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Album = () => {
-  return <h1>Album Gallary</h1>;
+const Album = (props) => {
+  return (
+    <>
+      <div className="album">
+        <img src={props.album_cover} alt="Album-cover" className="album_img" />
+        <div className="album_info">
+          <p>{props.album_title}</p>
+          <span className="album_category">{props.artist}</span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Album;
